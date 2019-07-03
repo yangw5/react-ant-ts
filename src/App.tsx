44f2694b7c48237aa70daf4f91f156e387c986ab
杨文伍 <yangw5@163.com>
2019-07-03 17:52:09 +0800

    新增：使用ant-react-hook+ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './component/Login';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    let a: number[] = [];
+    let b: number[] = [1, 2, 3];
+    a = b;
+    console.log(a);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Login />
+            </header>
+        </div>
+    );
+};
 
 export default App;
